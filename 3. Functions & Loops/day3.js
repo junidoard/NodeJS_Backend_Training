a07f90a541022eb9345fc8f7a1e252273eab4fe3
junidoard <junido21@gmail.com>
2025-08-11 15:18:58 +0700

    Add functions for multiplication and introduction, and implement counting and multiplication loops

---
+++ 3. Functions & Loops/day3.js
@@ -0,0 +1,23 @@
+function multiply(a, b) {
+    let result = a * b;
+    return result;
+}
+
+console.log(multiply(3, 4));
+
+function introduce(name,age){
+    let sentence = "My name is " + name + " and I am " + age + " years old.";
+    return sentence;
+}
+
+console.log(introduce("Junido", 29));
+
+let count = 1;
+while(count <= 10){
+    console.log(count);
+    count++;
+}
+
+for (let i = 1; i <= 10; i++) {
+    console.log(7*i);
+}
